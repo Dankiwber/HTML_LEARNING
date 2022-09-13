df63ca6748f941071279ee98d8689cfd53c105ff
Dankiwber <sonnefzo@gmail.com>
2022-09-13 14:37:28 -0600

    DFS demo code

--- Algorithm by python/Python_test_code.py
+++ Algorithm by python/Python_test_code.py
@@ -1,3 +1,18 @@
 a=1;b=2
 print("A is larger") if (a>b) else print("B is larger")
 print("Hello world")
+
+def DFS (sinit, Sg, T, B):
+	if n == Sg:
+		return True
+	if B<0:
+		return False
+	for n' in T(n)
+		if DFS (n', Sg, T, B-1):
+			return True
+	return False
+
+def IDDFS (Sinit, Sg, T):
+	B=0
+	while DEF(Sinit, Sg, T, B):
+		B=B+1
